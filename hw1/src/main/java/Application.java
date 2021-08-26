3f19255e4c93012947014cf84764cb9c709c5992
jabooticaba <jabuticaba600@gmail.com>
2021-08-27 01:23:25 +0700

    Kotik methods added

--- hw1/src/main/java/Application.java
+++ hw1/src/main/java/Application.java
@@ -6,9 +6,11 @@ public class Application {
         Kotik kotik1 = new Kotik(2, 90, "Barsik", "Miaow");
         Kotik kotik2 = new Kotik();
 
-        kotik1.getClass();
         kotik2.setKotik(3,95, "Princess", "Meow!");
+        kotik1.sleep();
+        System.out.println("Количество экземпляров класса Kotik: " + Kotik.getCounter());
+        System.out.println("Сытость kotik2: " + kotik2.getFullness());
+        kotik2.eat();
 
-        System.out.println(Kotik.counter);
     }
 }
--- hw1/src/main/java/model/Kotik.java
+++ hw1/src/main/java/model/Kotik.java
@@ -1,17 +1,18 @@
 package model;
 
+
 public class Kotik {
 
-    int weight;
-    int prettiness;
-    String name;
-    String meow;
+    private static int counter;
+    private int fullness = 5;
+    private int weight;
+    private int prettiness;
+    private String name;
+    private String meow;
 
-    public static int counter;
+    {counter++;}
 
-    public Kotik(){
-        counter++;
-    }
+    public Kotik() {}
 
     public Kotik(int weight, int prettiness, String name, String meow){
         this.weight = weight; this.prettiness = prettiness;
@@ -23,5 +24,74 @@ public class Kotik {
         this.prettiness = prettiness;
         this.name = name;
         this.meow = meow;
+    }
+
+    public int getFullness(){
+        return fullness;
+    }
+
+    public void eat(){ //TODO Сделать перегрузки метода
+    fullness++;
+    System.out.println("Кушает");
+    }
+
+    public boolean sleep(){
+        if (fullness > 0) {
+            System.out.println("Спит");
+            return true;
+        }else {
+            System.out.println("Котик хочет есть");
+            return false;
+        }
+    }
+
+    public boolean play(){
+        if (fullness > 0) {
+            System.out.println("Играет");
+            return true;
+        }else {
+            System.out.println("Котик хочет есть");
+            return false;
+        }
+    }
+
+    public boolean chaseMouse(){
+        if (fullness > 0) {
+            System.out.println("Преследует мышь");
+            return true;
+        }else {
+            System.out.println("Котик хочет есть");
+            return false;
+        }
+    }
+
+    public boolean lookingOutWindow(){
+        if (fullness > 0) {
+            System.out.println("Смотрит в окно");
+            return true;
+        }else {
+            System.out.println("Котик хочет есть");
+            return false;
+        }
+    }
+
+    public static int getCounter(){
+        return counter;
+    }
+
+    public int getWeight(){
+        return weight;
+    }
+
+    public int getPrettiness(){
+        return prettiness;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getMeow(){
+        return meow;
     }
 }
