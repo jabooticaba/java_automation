0742ee059ae8d06e21cff32318a8c35b51d07621
jabooticaba <jabuticaba600@gmail.com>
2021-08-26 19:46:36 +0700

    Class Application, Kotik (with metods) added

---
+++ hw1/src/main/java/model/Kotik.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Kotik {
+
+    int weight;
+    int prettiness;
+    String name;
+    String meow;
+
+    public static int counter;
+
+    public Kotik(){
+        counter++;
+    }
+
+    public Kotik(int weight, int prettiness, String name, String meow){
+        this.weight = weight; this.prettiness = prettiness;
+        this.name = name; this.meow = meow;
+    }
+
+    public void setKotik(int weight, int prettiness, String name, String meow){
+        this.weight = weight;
+        this.prettiness = prettiness;
+        this.name = name;
+        this.meow = meow;
+
+    }
+}
